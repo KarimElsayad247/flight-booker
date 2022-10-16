6e729d737d7a48639e1657599d86070771a0a21b
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-10-16 12:19:27 +0200

    Remove Devise from project
    Turns out I don't need authentication for this project. Just a simple
    passenger model.

--- db/schema.rb
+++ db/schema.rb
@@ -29,17 +29,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_14_142043) do
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_133254) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "username"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
